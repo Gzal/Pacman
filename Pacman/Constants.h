1d07ab95742ebff2f14b05954f1b935c0c0dcca0
Gzal <14190835+Gzal@users.noreply.github.com>
2018-02-06 20:15:26 -0800

    Create constants namespace and header file

---
+++ Pacman/Constants.h
@@ -0,0 +1,18 @@
+//
+//  Constants.h
+//  Pacman
+//
+//  Created by Jose Gabriel Gonzalez Belmontes on 2/6/18.
+//  Copyright © 2018 Jose Gabriel Gonzalez Belmontes. All rights reserved.
+//
+
+#include <SFML/System.hpp>
+
+#ifndef Constants_h
+#define Constants_h
+
+namespace consts {
+    sf::Vector2f window_sz{1024,1024};
+}
+
+#endif /* Constants_h */
