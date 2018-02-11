b6fd8509d1e2203572ac36cc986acc776d1b40f5
Gzal <14190835+Gzal@users.noreply.github.com>
2018-02-10 20:40:18 -0800

    Create 'Wall' class source and header files

---
+++ Pacman/Wall.hpp
@@ -0,0 +1,18 @@
+//
+//  Wall.hpp
+//  Pacman
+//
+//  Created by Jose Gabriel Gonzalez Belmontes on 2/10/18.
+//  Copyright © 2018 Jose Gabriel Gonzalez Belmontes. All rights reserved.
+//
+
+#ifndef Wall_hpp
+#define Wall_hpp
+
+#include <SFML/Graphics.hpp>
+
+Class wall {
+
+}
+
+#endif /* Wall_hpp */
