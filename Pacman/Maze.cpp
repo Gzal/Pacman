00c332471ecdbc3f859e95ae9b12fd6bbfee81e1
Gzal <14190835+Gzal@users.noreply.github.com>
2018-02-11 16:12:16 -0800

    Add 'Maze' class source and header files

---
+++ Pacman/Maze.cpp
@@ -0,0 +1,9 @@
+//
+//  Maze.cpp
+//  Pacman
+//
+//  Created by Jose Gabriel Gonzalez Belmontes on 2/11/18.
+//  Copyright © 2018 Jose Gabriel Gonzalez Belmontes. All rights reserved.
+//
+
+#include "Maze.hpp"
---
+++ Pacman/Maze.hpp
@@ -0,0 +1,12 @@
+//
+//  Maze.hpp
+//  Pacman
+//
+//  Created by Jose Gabriel Gonzalez Belmontes on 2/11/18.
+//  Copyright © 2018 Jose Gabriel Gonzalez Belmontes. All rights reserved.
+//
+
+#ifndef Maze_hpp
+#define Maze_hpp
+
+#endif /* Maze_hpp */
