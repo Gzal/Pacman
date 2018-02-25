593b99afdbe4aa93eeb1cf6f7204dd939bcb7d3b
Gzal <14190835+Gzal@users.noreply.github.com>
2018-02-24 20:02:35 -0800

    Add 'Ghost' class header and source files

---
+++ Pacman/Ghost.cpp
@@ -0,0 +1,9 @@
+//
+//  Ghost.cpp
+//  Pacman
+//
+//  Created by Jose Gabriel Gonzalez Belmontes on 2/24/18.
+//  Copyright © 2018 Jose Gabriel Gonzalez Belmontes. All rights reserved.
+//
+
+#include "Ghost.hpp"
---
+++ Pacman/Ghost.hpp
@@ -0,0 +1,14 @@
+//
+//  Ghost.hpp
+//  Pacman
+//
+//  Created by Jose Gabriel Gonzalez Belmontes on 2/24/18.
+//  Copyright © 2018 Jose Gabriel Gonzalez Belmontes. All rights reserved.
+//
+
+#ifndef Ghost_hpp
+#define Ghost_hpp
+
+
+
+#endif /* Ghost_hpp */
