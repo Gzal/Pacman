e964ea0fcbe9cf68c11af5e239e685573555d6a0
Gzal <14190835+Gzal@users.noreply.github.com>
2018-03-07 18:35:15 -0800

    Add 'Collider' class source and header files

---
+++ Pacman/Collider.cpp
@@ -0,0 +1,9 @@
+//
+//  Collider.cpp
+//  Pacman
+//
+//  Created by Jose Gabriel Gonzalez Belmontes on 3/7/18.
+//  Copyright © 2018 Jose Gabriel Gonzalez Belmontes. All rights reserved.
+//
+
+#include "Collider.hpp"
---
+++ Pacman/Collider.hpp
@@ -0,0 +1,14 @@
+//
+//  Collider.hpp
+//  Pacman
+//
+//  Created by Jose Gabriel Gonzalez Belmontes on 3/7/18.
+//  Copyright © 2018 Jose Gabriel Gonzalez Belmontes. All rights reserved.
+//
+
+#ifndef Collider_hpp
+#define Collider_hpp
+
+#include <stdio.h>
+
+#endif /* Collider_hpp */
